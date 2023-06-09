376531c6cbd5790fe371f1e6fa100f6aa5b99ffc
huybang2017 <ndhbang2017@gmail.com>
2023-06-10 01:47:49 +0700

    fixing

--- src/components/MainPage/MainPageHeader/MainPageHeader.jsx
+++ src/components/MainPage/MainPageHeader/MainPageHeader.jsx
@@ -1,43 +1,11 @@
 import React, { useRef, useState, useEffect } from "react";
 import { DiJira } from "react-icons/di";
 import styles from "./MainPageHeader.module.scss";
-import "./MainPageHeader.scss";
 import { Container, Dropdown } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { Navigate, useSearchParams } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import { signout } from "../../../slice/userslice";
-import Avatar from "@mui/material/Avatar";
-import DropdownButton from "react-bootstrap/DropdownButton";
-
-function stringToColor(string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = "#";
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
-}
-
-function stringAvatar(name) {
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: `${name.charAt(0).toUpperCase()}`,
-  };
-}
 
 function MainPageHeader() {
   const [name, setName] = useState("");
@@ -68,9 +36,7 @@ function MainPageHeader() {
         </div>
         <div>
           <Dropdown>
-            <Dropdown.Toggle>
-              <Avatar {...stringAvatar(`${name}`)} />
-            </Dropdown.Toggle>
+            <Dropdown.Toggle variant="success">{name}</Dropdown.Toggle>
 
             <Dropdown.Menu>
               <Dropdown.Item href="#/action-2">Setting</Dropdown.Item>
