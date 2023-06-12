f23f22e4305925453ff4c8929c7f87249c51d48e
huybang2017 <ndhbang2017@gmail.com>
2023-06-13 00:47:12 +0700

    fixing

--- src/components/MainPage/MainPageBodyRight/MainPageBodyRight.jsx
+++ src/components/MainPage/MainPageBodyRight/MainPageBodyRight.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import CardMain from "../../CardMain/CardMain";
 
 function MainPageBodyRight() {
   return (
-    <>
+    <div
+      style={{ display: "flex", gap: "10px", height: "100%", width: "100%" }}
+    >
       <CardMain />
-    </>
+      <CardMain />
+    </div>
   );
 }
 
