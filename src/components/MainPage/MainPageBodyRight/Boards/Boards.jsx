3f67a54589c6b7fae399f8c50d84589fff02d563
tyasenpai <tantai204.10@gmail.com>
2023-06-15 12:14:14 +0700

    Board new update 15/6

--- src/components/MainPage/MainPageBodyRight/Boards/Boards.jsx
+++ src/components/MainPage/MainPageBodyRight/Boards/Boards.jsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 import style from "./Boards.module.scss";
 import CardMain from "../../../CardMain/CardMain";
 import { apigetProject } from "./../../../../apis/projectAPI";
-import Loader from "../../../Loader/Loader";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 // Lấy tên user từ phía client localstorage
 function Boards() {
   const [dataproject, setdataproject] = useState([]);
   useEffect(() => {
-    apigetProject("ab")
+    apigetProject("Vũ Lê Bảo Long")
       .then((res) => {
         setdataproject(res.content);
       })
@@ -16,18 +16,26 @@ function Boards() {
       });
   }, []);
   return (
+  <DragDropContext>
     <div className="container-fluid">
+    
       <div className="row">
         {dataproject.length > 0 ? (
           dataproject.map((value, index) => {
-            console.log(value);
-            return <CardMain value={value} />;
-          })
-        ) : (
-          <Loader />
+            return(
+              <Droppable droppableId={index}>
+                {(provided)=>(
+                <CardMain className="drag"  {...provided.droppableProps} {...provided.droppableProps} ref={provided.innerRef} value={value} index={index}/>
+                )}
+              </Droppable>  
+              )}
+            ) 
+          ) : (
+          <img className={style.img} src="./img/nodatafound.jpg"/>
         )}
       </div>
     </div>
+    </DragDropContext>  
   );
 }
 
